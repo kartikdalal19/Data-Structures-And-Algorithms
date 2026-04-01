dca89bd875c93ad2f77e5f2407e1fb9a525d87f1
kartikdalal19 <dalalkartik077@gmail.com>
2026-04-01 22:57:22 +0530

    Add files via upload

---
+++ Maths/GP.java
@@ -0,0 +1,59 @@
+public class GP {
+ 
+    public static double gp(int a , int b, int n)
+{
+    if(a==0)
+   {
+     return 0;
+   }
+
+
+    double r = (double) b / a;
+    
+    return a*((int)Math.pow(r, n-1));
+
+}
+
+    public static void main(String[] args) {
+ int a=84;
+ int b=87;
+ int n=3;
+ 
+ double answer= gp(a, b, n);
+
+ System.out.println(answer);
+    }
+}
+
+
+
+// Given the first 2 terms A and B of a Geometric Series. The task is to find the Nth term of the series.
+
+// Example 1:
+
+// Input:
+// A = 2 
+// B = 3
+// N = 1
+// Output: 2
+// Explanation: The first term is already
+// given in the input as 2
+// Example 2:
+
+// Input:
+// A = 1
+// B = 2
+// N = 5
+// Output: 16
+// Explanation: Common ratio = 2,
+// Hence Fifth term is 1*(2)(5-1) = 24 = 16 .
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function termOfGP() that takes A, B and N as parameter and returns Nth term of GP. The return value should be double that would be automatically converted to floor by the driver code.
+
+// Expected Time Complexity : O(logN)
+// Expected Auxilliary Space : O(1)
+
+// Constraints:
+// -100 <= A <= 100
+// -100 <= B <= 100
+// 1 <= N <= 5
